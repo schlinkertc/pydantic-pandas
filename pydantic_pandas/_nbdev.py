7af81b14155b239691a1588609dd29750956aeb8
Charlie Schlinkert <schlinkertc@gmail.com>
2021-09-30 18:05:43 -0400

    clean and build

--- pydantic_pandas/_nbdev.py
+++ pydantic_pandas/_nbdev.py
@@ -2,20 +2,16 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"TypedArray": "00_core.ipynb",
+index = {"DataFrame": "00_core.ipynb",
+         "BaseModel": "00_core.ipynb",
+         "TypedArray": "00_core.ipynb",
          "ArrayMeta": "00_core.ipynb",
          "ConstrainedArray": "00_core.ipynb",
-         "update_not_none": "00_core.ipynb",
+         "BaseFrame": "00_core.ipynb",
          "TypedFrame": "00_core.ipynb",
          "FrameMeta": "00_core.ipynb",
          "ConstrainedFrame": "00_core.ipynb",
          "conframe": "00_core.ipynb",
-         "Array": "00_core.ipynb",
-         "conarray": "00_core.ipynb",
-         "PandasDataFrame": "00_core.ipynb",
-         "extract_ddf_from_model_fields": "00_core.ipynb",
-         "DataFrameModelMetaclass": "00_core.ipynb",
-         "BaseFrame": "00_core.ipynb",
          "delegates": "98_utils.ipynb"}
 
 modules = ["core.py",
